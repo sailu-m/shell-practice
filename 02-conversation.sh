4cf7f7498bd75e7de23b423a3132617e282ab828
Sailu M <sailu123@gmail.com>
2026-07-30 20:34:55 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sailu:: Hello Siri, How are you?"
+echo "Siri:: Hi Sailu, I am fine thanks for asking, How are you doing?"
+echo "Sailu:: doing fine, what are you learning Siri?"
+echo "Siri:: I am learning shell. What about you?"
