6b561ae7d4e714e69ac7e7f3891ad699fd23a9f2
Sailu M <sailu123@gmail.com>
2026-08-07 19:25:57 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,13 +4,13 @@ TIMESTAMP=$(date)
 
 echo "Script executed at:" $TIMESTAMP
 
-START_TIME=$(date t%s)
+START_TIME=$(date +%s)
 
 echo "Script executed at: $START_TIME"
 
 sleep 10
 
-END_TIME=$(date t%s)
+END_TIME=$(date +%s)
 TOTAL_TIME=$((END_TIME-$START_TIME))
 
 echo "Script executed in: $TOTAL_TIME"
