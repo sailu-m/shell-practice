157e7c97de0753616fba76e341d4cd158dc4e8ab
Sailu M <sailu123@gmail.com>
2026-08-07 20:08:12 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,5 +5,13 @@ NUM2=200
 
 SUM=$((NUM1+NUM2))
 
-echo "Sun is: $SUM"
+echo "Sum is: $SUM"
 
+#Array
+FRUITS=("Apple", "Banana", "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
