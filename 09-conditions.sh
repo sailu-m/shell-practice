01db4d5057db2867e3e591b12bc969b039c49129
Sailu M <sailu123@gmail.com>
2026-08-07 20:13:31 +0530

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal 
+if [$NUMEBER -gt 20]; then
+    echo "Given number: $NUMBER is greater than 20"
+if [$NUMEBER -eq 20]; then
+    echo "Given number: $NUMBER is equal to 20"
+else
+    echo "Given number: $NUMBER is less than 20"
+fi 
