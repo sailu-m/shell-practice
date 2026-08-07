29753b7332e388030abe872b1f9d9fcb4974f56b
Sailu M <sailu123@gmail.com>
2026-08-07 19:20:25 +0530

    actions

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Sailu
+PERSON2=Siri
+
+echo "please enter your username::"
+read USER_NAME
+
+echo "Username is $USER_NAME"
+echo "please enter your password::"
+read -s PASSWORD
