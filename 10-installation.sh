ab4f4639239048e833544229612483a1b235487e
Sailu M <sailu123@gmail.com>
2026-08-08 19:17:04 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -8,3 +8,25 @@ fi
 
 echo "Installing nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx: ...FAILURE"
+else
+    echo "Installing Nginx: ...SUCCESS"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Mysql....FAILURE"
+else
+    echo "Installing Mysql....SUCCESS"
+fi
+dnf install nodejs -y
+
+if [ ?$ -ne 0 ]; then
+    echo "Installing nodejs....FAILURE"
+    exit 1
+else 
+    echo "Installing nodejs....SUCCESS"
+fi
