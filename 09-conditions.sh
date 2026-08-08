6d50b5a0bc6eeac1a7ed8441c17545a250f599b3
Sailu M <sailu123@gmail.com>
2026-08-08 19:00:10 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -8,9 +8,9 @@
 NUMBER=20
 
 
-if [$NUMEBER -gt 20]; then
+if [ $NUMEBER -gt 20 ]; then
     echo "Given number:: $NUMBER is greater than 20"
-if [$NUMEBER -eq 20]; then
+if [ $NUMEBER -eq 20 ]; then
     echo "Given number:: $NUMBER is equal to 20"
 else
     echo "Given number:: $NUMBER is less than 20"
