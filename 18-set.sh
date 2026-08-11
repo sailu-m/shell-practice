6cbf11c063678e5459e28fed6646e8d1959a7782
Sailu M <sailu123@gmail.com>
2026-08-11 23:11:23 +0530

    actions

--- 18-set.sh
+++ 18-set.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
 set -e #this will be checking for errors, if errors it will exit
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo "$2...$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ #sudo sh 14-loops.sh nginx mysql nodejs
+
+do
+    dnf list installed $package &>>LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not instalaled, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package already installed, $Y skipping $N"
+    fi
+done
